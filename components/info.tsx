1a50ec1738e82d2f9c54e79e38bca52bf6468dd8
Farhan Khorsheed <ratul097@gmail.com>
2023-07-18 18:44:25 +1000

    [UPDATED] Add to cart functionality to product and preview

--- components/info.tsx
+++ components/info.tsx
@@ -5,12 +5,19 @@ import { ShoppingCart } from 'lucide-react'
 import { Product } from '@/types';
 import Currency from '@/components/ui/currency';
 import Button from '@/components/ui/button';
+import useCart from '@/hooks/use-cart';
 
 interface InfoProps {
   data: Product;
 }
 
 const Info: React.FC<InfoProps> = ({ data }) => {
+  const cart = useCart()
+
+  const onAddToCart = () => {
+    cart.addItem(data)
+  }
+
   return (
     <div>
       <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
@@ -35,6 +42,7 @@ const Info: React.FC<InfoProps> = ({ data }) => {
       </div>
       <div className="mt-10 flex items-center gap-x-3">
         <Button 
+          onClick={onAddToCart}
           className="flex items-center gap-x-2"
         >
           Add To Cart
